de2062bb708778c1350fc9bba1a63d5aeba55e71
vbaymurzin <vbaymurzin@diasoft.ru>
2021-04-09 19:14:15 +0500

    Правки по советам из пуллреквеста

--- homework01/src/main/java/ru/otus/vbaymurzin/config/AppConfig.java
+++ homework01/src/main/java/ru/otus/vbaymurzin/config/AppConfig.java
@@ -6,25 +6,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import ru.otus.vbaymurzin.dao.TestQuestionDao;
 import ru.otus.vbaymurzin.dao.TestQuestionDaoCSV;
 
-// Вопрос: нужно ли писать тестя на конфигурационные классы?
+// Вопрос: нужно ли писать тест на конфигурационные классы?
 @Configuration
 @PropertySource("application.properties")
 @RequiredArgsConstructor
 public class AppConfig {
-    @Value("${questions-csv}")
-    String csvFileName;
 
-    private final ResourceLoader resourceLoader;
-
-    // как раз сэмулировалоась ситуация, когда не можем поменять класс, чтобы он принимал имя файла, а не ресурс
-    // можно было бы, имя файла сразу передавать в конструктор класса и тогда мы бы его аннотировали как @Repository
     @Bean
-    public TestQuestionDao testQuestionDaoCSV() {
-        Resource resource = resourceLoader.getResource(csvFileName);
+    public TestQuestionDao testQuestionDaoCSV(@Value("${questions-csv}") Resource resource) {
         return new TestQuestionDaoCSV(resource);
     }
 }
